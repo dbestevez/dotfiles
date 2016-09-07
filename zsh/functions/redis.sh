01795242857ada2625633965c4d1baae0ae32e9b
Diego Blanco Estévez <diegoblaes@gmail.com>
2016-09-07 14:14:10 +0200

    Add a fuction to get all redis values by pattern

--- zsh/functions/redis.sh
+++ zsh/functions/redis.sh
@@ -1,3 +1,19 @@
+# Get values for the list of keys
+function redis-get() {
+    keys=($(redis-search $1));
+    i=1;
+
+    for key in $keys; do
+        if [[ $i -gt 1 ]] then;
+            echo "";
+        fi
+
+        echo $i")" $key
+        redis-cli get $key;
+        i=$(( $i + 1 ));
+    done;
+}
+
 # List all redis-keys
 function redis-list() {
     redis-cli keys "*";
@@ -14,7 +30,8 @@ function redis-delete() {
 }
 
 # Alias
-alias rdl='redis-list'
-alias rds='redis-search'
 alias rdd='redis-delete'
 alias rdf='redis-cli flushall'
+alias rdg='redis-get'
+alias rdl='redis-list'
+alias rds='redis-search'
