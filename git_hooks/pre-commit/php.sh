3565f5abb5ff275bc6e8d80ecd58e5fd777d5074
Diego Blanco Estévez <dbestevez@protonmail.com>
2017-03-24 16:58:49 +0100

    Update and add more git hooks

--- git_hooks/pre-commit/php.sh
+++ git_hooks/pre-commit/php.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# This hook checks debug messages in staged files and aborts the commit when one
-# or more debug messages are found.
+# This hook checks php syntax in staged files and aborts the commit when one
+# or more errors are found.
 
-staged=`git diff --cached --name-only`
+staged=`git diff --cached --name-only | grep ".php$"`
 
 # Nothing to do
 if [[ $staged = '' ]]; then
     exit 0
 fi
 
-echo -e -n "\033[1m==> Checking debug messages in staged files...\033[0m "
-
-pattern="^\s*(var_dump|console.log)"
+echo -e -n "\033[1m==> Checking PHP syntax in staged files...\033[0m "
 
 files=()
 counter=0
 for file in $staged; do
-    if [[ -f $file && `ag $pattern $file` != '' ]]; then
+    if [[ -f $file && ! `php -l $file 2>&1` =~ 'No syntax errors detected.*' ]]; then
         counter=$((counter + 1))
         files+=($file)
     fi
@@ -29,7 +27,7 @@ if [[ $counter = 0 ]]; then
 fi
 
 echo -e "\E[31;5mFAIL\033[0m"
-echo -e "\E[33;5mDebug messages found in:\033[0m"
+echo -e "\E[33;5mSyntax errors found in:\033[0m"
 
 for file in "${files[@]}"; do
     echo -e " \E[34;5m$file\033[0m"
