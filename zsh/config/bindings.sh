c1a64e3420f57ad605a7a0f51a53978b5059c1ce
Diego Blanco Estévez <diegoblaes@gmail.com>
2017-10-25 10:52:28 +0200

    Add zsh-snippets and update bindings

--- zsh/config/bindings.sh
+++ zsh/config/bindings.sh
@@ -1,8 +1,10 @@
 # zsh-users/zsh-autosuggestions
-bindkey '^ ' autosuggest-accept
 bindkey -M vicmd ' ' autosuggest-accept
 bindkey -M vicmd 'L' autosuggest-accept
 
 # zsh-users/zsh-history-substring-search
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
+
+bindkey '^ ' snippet-expand
+bindkey '^F' fuck-command-line
