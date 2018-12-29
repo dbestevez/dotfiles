342d0ea08fdcb843dd487945c0a5b83be6edd448
Diego Blanco Estévez <dbestevez@protonmail.com>
2018-12-29 18:20:15 +0100

    Improve auto-detect.sh scripts

--- src/config/zsh/config/environments/auto-detect.sh
+++ src/config/zsh/config/environments/auto-detect.sh
@@ -4,10 +4,10 @@
 export ETH=$(ifconfig | grep enp | cut -d ':' -f1)
 
 # Battery name
-export POWER=`ls /sys/class/power_supply | grep AC`
+export POWER=$(ls /sys/class/power_supply | grep AC)
 
 # Path to temperature source
-export TMP="$(find /sys/devices -name temp1_input | head -n 1)"
+export TMP=$(find /sys/devices -name temp1_input | head -n 1)
 
 # Wireless internet interface for polybar
 export WLAN=$(ifconfig | grep wlp | cut -d ':' -f1)
