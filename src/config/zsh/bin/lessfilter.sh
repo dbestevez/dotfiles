6571445e7d3e47f84a355e690f152e7d90c279a1
Diego Blanco Estévez <dbestevez@protonmail.com>
2019-07-03 01:20:47 +0200

    Add scripts and update PATH

---
+++ src/config/zsh/bin/lessfilter.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+case "$1" in
+    *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
+    *.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
+    *.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
+    *.diff|*.patch|*.py|*.rb|*.sql|*.ebuild|*.eclass)
+        pygmentize "$1";;
+    *.json)
+        cat "$1" | json_reformat | pygmentize -l json;;
+
+    .bashrc|.bash_aliases|.bash_environment)
+        pygmentize -l sh "$1"
+        ;;
+    *)
+        grep "#\!/bin/bash" "$1" > /dev/null
+        if [ "$?" -eq "0" ]; then
+            pygmentize -l sh "$1"
+        else
+            exit 1
+        fi
+esac
+
+exit 0
