03af17b2aa7c738194f85165269e27d07d050918
Diego Blanco Estevez <diegoblaes@gmail.com>
2020-11-15 19:26:39 +0100

    Clean scripts

--- src/config/zsh/functions/volume.sh
+++
@@ -1,8 +0,0 @@
-vol() {
-    sinks=$(pacmd list-sinks | grep index | sed -e "s/.*index:\s\+//g")
-    volume=$(((65536 * $1 + 99) / 100))
-
-    for sink in $(echo $sinks); do
-        pacmd set-sink-volume $sink $volume
-    done
-}
