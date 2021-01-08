117f4b9f5e08a4f8d8b2873c82a582513462ef0e
Diego Blanco Estevez <diegoblaes@gmail.com>
2021-01-09 00:44:47 +0100

    Add 1ambda/zsh-snippets

--- src/config/zsh/config/bindings.sh
+++ src/config/zsh/config/bindings.sh
@@ -1,3 +1,10 @@
+# oh-my-zsh/plugins/vi-mode
+bindkey -M vicmd 'e' edit-command-line
+
+# 1ambda/zsh-snippets
+bindkey '^s' zsh-snippets-widget-list
+bindkey '^x' zsh-snippets-widget-expand
+
 # zsh-users/zsh-autosuggestions
 bindkey -M vicmd ' ' autosuggest-accept
 bindkey -M vicmd 'L' autosuggest-accept
@@ -5,7 +12,3 @@ bindkey -M vicmd 'L' autosuggest-accept
 # zsh-users/zsh-history-substring-search
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
-
-bindkey -M vicmd 'e' edit-command-line
-
-bindkey '^ ' snippet-expand
