34d15ca83153f47cf2fedaf44c3dffc2c9739477
Diego Blanco Estevez <diegoblaes@gmail.com>
2021-01-09 00:06:36 +0100

    Remove custom vi-mode

--- src/config/zsh/config/plugins.sh
+++ src/config/zsh/config/plugins.sh
@@ -1,3 +1,7 @@
 # zsh-users/zsh-autosuggestions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=30'
 ZSH_AUTOSUGGEST_STRATEGY='match_prev_cmd'
+
+# oh-my-zsh/plugins/vi-mode
+VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=false
+VI_MODE_SET_CURSOR=false
