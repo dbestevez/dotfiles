661f874a6389a9c64e97fdac7144656ceb6fc491
Diego Blanco Estévez <dbestevez@protonmail.com>
2021-01-10 01:19:10 +0100

    Add zsh-snippets

--- src/config/zsh/config/bindings.sh
+++ src/config/zsh/config/bindings.sh
@@ -1,9 +1,8 @@
-# oh-my-zsh/plugins/vi-mode
-bindkey -M vicmd 'e' edit-command-line
-
-# 1ambda/zsh-snippets
-bindkey '^s' zsh-snippets-widget-list
-bindkey '^x' zsh-snippets-widget-expand
+# dbestevez/zsh-snippets
+bindkey '^S^J' zsh-snippets-widget-list
+bindkey '^S^L' zsh-snippets-widget-expand
+bindkey '^S^K' zsh-snippets-widget-search
+bindkey '^S^H' zsh-snippets-widget-jump
 
 # zsh-users/zsh-autosuggestions
 bindkey -M vicmd ' ' autosuggest-accept
