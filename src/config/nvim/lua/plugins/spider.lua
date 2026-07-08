98931af69f482e89f401e5ae50dfb719c233c650
Diego Blanco Estévez <dbestevez@protonmail.com>
2026-07-08 17:46:44 +0200

    Keep old word text object behavior in spider

--- src/config/nvim/lua/plugins/spider.lua
+++ src/config/nvim/lua/plugins/spider.lua
@@ -10,3 +10,4 @@ vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<C
 vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>")
 vim.keymap.set({ 'n', 'o', 'x' }, 'ge', "<cmd>lua require('spider').motion('ge')<CR>")
 vim.keymap.set('n', 'cw', 'ce', { remap = true })
+vim.keymap.set('n', 'dw', 'de', { remap = true })
