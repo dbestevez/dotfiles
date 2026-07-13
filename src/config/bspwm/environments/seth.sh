777e7ce6bdedd6dd5c604f18e3768624505e656e
Diego Blanco Estévez <dbestevez@protonmail.com>
2026-07-13 08:48:14 +0200

    Remove old configurations and scripts

--- src/config/bspwm/environments/seth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PRIMARY_INDEX=$(mons | grep " $PRIMARY " | cut -d':' -f1)
-SECONDARY_INDEX=$(mons | grep " $SECONDARY " | cut -d':' -f1)
-
-mons -S $SECONDARY_INDEX,$PRIMARY_INDEX:R
-
-bspc monitor $PRIMARY   -d 1 2 3
-bspc monitor $SECONDARY -d 4 5 6
--- src/local/bin/xmenu-button.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while true; do
-  echo "  "
-  sleep 30 &
-  wait
-done
